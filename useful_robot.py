08c1ce542b2bc01aa43b5a751e5e20775e096e08
Cosmin Velea <Cosmin.Velea@gdit.com>
2019-12-25 20:04:16 -0500

    added recklessness

--- useful_robot.py
+++ useful_robot.py
@@ -1,9 +1,13 @@
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 
 left_motor = LargeMotor(OUTPUT_A)
 right_motor = LargeMotor(OUTPUT_B)
-
+helicopter = MediumMotor(OUTPUT_C)
 
 left_motor.on_for_degrees(speed=50, degrees=3200, brake=True, block=False)
-right_motor.on_for_degrees(speed=50, degrees=3200, brake=True, block=True)
+right_motor.on_for_degrees(speed=50, degrees=3200, brake=True, block=False)
+
+helicopter.on_for_seconds(speed=75, seconds=10, brake=False, block=False)
+
+
 
