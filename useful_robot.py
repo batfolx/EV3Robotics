1569e28a8d1c2aae24496763d0622ed2ebc840b0
Cosmin Velea <Cosmin.Velea@gdit.com>
2019-12-27 00:53:45 -0500

    changed design and methods

--- useful_robot.py
+++ useful_robot.py
@@ -1,13 +1,29 @@
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
-
-left_motor = LargeMotor(OUTPUT_A)
-right_motor = LargeMotor(OUTPUT_B)
-helicopter = MediumMotor(OUTPUT_C)
-
-left_motor.on_for_degrees(speed=50, degrees=3200, brake=True, block=False)
-right_motor.on_for_degrees(speed=50, degrees=3200, brake=True, block=False)
-
-helicopter.on_for_seconds(speed=75, seconds=10, brake=False, block=False)
+from ev3dev2.sensor.lego import UltrasonicSensor
+from ev3dev2.sensor import INPUT_1
 
 
+def check_distance(eyes: UltrasonicSensor):
+    pass
 
+
+def forward(left_motor: LargeMotor, right_motor: LargeMotor, seconds=0.5):
+    left_motor.on_for_seconds(speed=50, seconds=seconds, brake=True, block=False)
+    right_motor.on_for_seconds(speed=50, seconds=seconds, brake=True, block=False)
+
+
+def stop(left_motor: LargeMotor, right_motor: LargeMotor):
+    left_motor.off()
+    right_motor.off()
+
+
+def main():
+    left_motor = LargeMotor(OUTPUT_A)
+    right_motor = LargeMotor(OUTPUT_B)
+    bulldozer = MediumMotor(OUTPUT_C)
+    eyes = UltrasonicSensor(INPUT_1)
+    forward(left_motor, right_motor, seconds=5)
+    print("Blocking not caught we can still check for distance and stop as needed")
+
+
+main()
