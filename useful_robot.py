b974f45366fc3a4b91974b34b9b2eb8cb7a9a5cd
Cosmin Velea <Cosmin.Velea@gdit.com>
2019-12-27 01:23:03 -0500

    autonomous try number 1

--- useful_robot.py
+++ useful_robot.py
@@ -1,6 +1,8 @@
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor.lego import UltrasonicSensor
 from ev3dev2.sensor import INPUT_1
+from ev3dev2.button import Button
+import random
 
 
 def check_distance(eyes: UltrasonicSensor):
@@ -22,19 +24,43 @@ def backward(left_motor: LargeMotor, right_motor: LargeMotor, seconds=0.5, speed
     right_motor.on_for_seconds(speed=-speed, seconds=seconds, brake=True, block=False)
 
 
+def knock_o_matic(bulldozer: MediumMotor, seconds=3, speed=75):
+    bulldozer.on_for_seconds(speed=speed, seconds=seconds, brake=False, block=False)
+
+
+def left_and_right(left_motor: LargeMotor, right_motor: LargeMotor, seconds=0.5, speed=50):
+    left_motor.on_for_seconds(speed=speed, seconds=seconds, block=False)
+    right_motor.on_for_seconds(speed=speed / 2, seconds=seconds, block=True)
+    left_motor.on_for_seconds(speed=speed / 2, seconds=seconds, block=False)
+    right_motor.on_for_seconds(speed=speed, seconds=seconds, block=True)
+
+
+def turn_left(left_motor: LargeMotor):
+    left_motor.on_for_degrees(speed=100, degrees=180, block=True)
+
+
 def main():
     left_motor = LargeMotor(OUTPUT_A)
     right_motor = LargeMotor(OUTPUT_B)
     bulldozer = MediumMotor(OUTPUT_C)
     eyes = UltrasonicSensor(INPUT_1)
+    button = Button()
 
-    forward(left_motor, right_motor, seconds=15)
+    forward(left_motor, right_motor, seconds=60)
     print("Blocking not caught we can still check for distance and stop as needed")
     while True:
         if check_distance(eyes):
             print("Detected an object in front of me.")
+            knock_o_matic(bulldozer)
+            left_and_right(left_motor, right_motor)
+            backward(left_motor, right_motor, seconds=2, speed=75)
+            turn_left(left_motor)
             stop(left_motor, right_motor)
-            return
+            forward(left_motor, right_motor, seconds=60)
+
+        if button.any():
+            stop(left_motor, right_motor)
+            break
 
 
 main()
